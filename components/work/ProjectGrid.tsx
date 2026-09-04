a707cafa7c8a6c1004822f5fbd16400bc0e65f54
Mohsen Hasanpour <hasanpour.photo@gmail.com>
2026-09-04 22:02:08 +0330

    refactor: migrate project card and grid base styles to CSS modules

--- components/work/ProjectGrid.tsx
+++ components/work/ProjectGrid.tsx
@@ -1,5 +1,6 @@
 import type { FeaturedProject } from "@/content/projects";
 import { ProjectCard } from "./ProjectCard";
+import styles from "./ProjectGrid.module.css";
 
 type ProjectGridProps = {
   projects: FeaturedProject[];
@@ -8,7 +9,7 @@ type ProjectGridProps = {
 
 export function ProjectGrid({ projects, showNumbers = true }: ProjectGridProps) {
   return (
-    <div className="project-grid">
+    <div className={`${styles.projectGrid} project-grid`}>
       {projects.map((project, index) => (
         <ProjectCard key={project.slug} project={project} number={index + 1} showNumber={showNumbers} />
       ))}
